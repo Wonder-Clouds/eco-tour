7ba5bed8fa400e6e2dbc02a2eff226bb3902a8aa
zsalazarq <salazarsergio1082@gmail.com>
2025-08-02 10:44:22 -0500

    add initial routes

--- eco-hub/src/app/app.routes.ts
+++ eco-hub/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './modules/home/home';
+import { Services } from './modules/services/services';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: '/servicios', component: Services },
+];
