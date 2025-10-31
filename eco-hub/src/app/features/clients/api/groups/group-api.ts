8e470cae3d0af5cc0a07298155fa34c2c08f8140
Sergio Salazar <salazarsergio1082@gmail.com>
2025-10-31 03:37:22 -0500

    Añadidos grupos

---
+++ eco-hub/src/app/features/clients/api/groups/group-api.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { PaginatedResponse } from '../../../../models/PaginatedResponse';
+import { Observable } from 'rxjs';
+import { Group } from '../../types/Group';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GroupApi {
+  private baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  getGroups(): Observable<PaginatedResponse<Group>> {
+    return this.http.get<PaginatedResponse<Group>>(`${this.baseUrl}/group`);
+  }
+}
